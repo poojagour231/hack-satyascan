cae961bd1e51bcaa6577e68f2227bf7903b3e831
Pooja gour <poojagour231@gmail.com>
2026-04-12 15:22:05 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.user_signup),
-    path('login/',views.user_login,name='login'),
-    path('signup/',views.user_signup,name='signup'),
-    path('dashboard/',views.dashboard,name='dashboard'),
-]
